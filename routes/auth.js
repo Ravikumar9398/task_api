e502c7526af4cd780f10faf7ab22541b1ed687ce
Ravikumar9398 <ravikumar.yesupogu9@gmail.com>
2024-05-01 18:22:09 +0530

    API tasks

--- routes/auth.js
+++ routes/auth.js
@@ -10,7 +10,7 @@ require("dotenv/config");
 // creating jwt token
 
 const createToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET);
+  return jwt.sign({ id }, process.env.JWTSECRET);
 };
 
 // Post method fro new user credentials
