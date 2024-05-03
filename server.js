43a96b58dd3fbc7cbd395f3e043fbb3062b63984
Ravikumar9398 <ravikumar.yesupogu9>
2024-05-03 22:42:11 +0530

    update server

--- server.js
+++ server.js
@@ -9,12 +9,12 @@ app.use(express.json());
 // connecting to database
 require("./config/config");
 
-app.use("/user", auth);
-app.use("/tasks", tasks);
-
 app.get("/", (req, res) => {
   res.send("For API Data Went to End Point ( / tasks / )  ");
 });
+
+app.use("/user", auth);
+app.use("/tasks", tasks);
 
 app.listen(3000, () => {
   console.log("Example app listening on port 3000!");
